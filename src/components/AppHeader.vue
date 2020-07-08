281331080b6892f008bd88d2e6c39a2dee1195a8
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2020-07-08 19:32:42 +0300

    First commit of my little e-shop + added header and navigation drawer

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,64 @@
+<template>
+  <section>
+    <v-app-bar
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Ciprian's store</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+          center
+          solo-inverted
+          flat
+          hide-details
+          label="Search"
+          prepend-inner-icon="search"
+        ></v-text-field>
+      <v-spacer></v-spacer>
+      <v-btn icon
+        v-for='option in headerOptions'
+        v-bind:key='option'>
+        <v-icon>{{ option }}</v-icon>
+      </v-btn>
+    </v-app-bar>
+    
+
+     <v-navigation-drawer
+        v-model="drawer"
+        app
+        temporary
+      >
+        <v-list dense>
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-email</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Contact</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+  </section>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+      headerOptions: ['account_circle', 'favorite', 'local_mall']
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#FF9800",
+        secondary: "#FFC107",
+        accent: "#37474F",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107"
+      }
+    }
+  }
+});
