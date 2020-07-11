cf9064c5d6907e5c452777dc1d62f96c54314c77
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2020-07-11 21:53:26 +0300

    Crafted footer component

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,20 @@
     <v-main>
       <router-view/>
     </v-main>
+    <AppFooter/>
   </v-app>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader"
+import AppFooter from "./components/AppFooter"
 
 export default {
   name: "App",
 
   components: {
-    AppHeader
+    AppHeader,
+    AppFooter
   },
 
   data: () => ({
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-footer
+    color="primary lighten-1"
+    padless
+    class="mt-15"
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white"
+        text
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="primary lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        <strong>{{ new Date().getFullYear() }} — Made &amp; design by Ciprian Ursulean</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Terms and conditions',
+        'Orders and shipment',
+        'About us',
+        'Client support',
+        'Contact Us',
+      ],
+    }),
+  }
+</script>
