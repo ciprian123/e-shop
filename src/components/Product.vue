a1028bbb541a3b24656861d4570dc00b66e73950
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2020-07-12 17:29:20 +0300

    Product component improvements and crafted some home catalog layout

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -8,20 +8,27 @@
       height="140px"
     ></v-img>
 
-
     <v-card-title class="accent--text">
       {{ name }}
     </v-card-title>
 
     <v-card-actions>
-      <v-btn text icon><v-icon>local_mall</v-icon></v-btn>
+      <v-btn text icon><v-icon class="accent--text">local_mall</v-icon></v-btn>
       <v-spacer></v-spacer>
-      <v-card-title class="primary--text justify-center">
-      {{ price }}
+      <v-card-title class="accent--text justify-center">
+        {{ price }}
       </v-card-title>
       <v-spacer></v-spacer>
       <v-btn icon>
-        <v-icon>favorite</v-icon>
+        <v-icon class="accent--text">favorite</v-icon>
+      </v-btn>
+    </v-card-actions>
+    <v-card-actions class="justify-center">
+      <v-btn
+        color="primary"
+        class="mb-2"
+      >
+        Show details
       </v-btn>
     </v-card-actions>
   </v-card>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,38 @@
 <template>
-  <v-col cols="12">
-    <Product name="Bormasina" price="123 RON" image="https://picsum.photos/240/140"/>
-    <Product name="Smart TV" price="234 RON" image="https://picsum.photos/240/140"/>
-  </v-col>
+  <v-container fluid>
+    <v-row 
+      align="center"
+      justify="start"
+    >
+    <v-col cols="3" md="3" sm="6" xs="6">
+      <Product name="Bormasina" price="123 RON" image="https://picsum.photos/240/140"/>
+    </v-col>
+    <v-col cols="3" md="3" sm="6" xs="6">
+      <Product name="Smart TV" price="234 RON" image="https://picsum.photos/240/140"/>
+    </v-col>
+    <v-col cols="3" md="3" sm="6" xs="6">
+      <Product name="Smart Fridge" price="1000 RON" image="https://picsum.photos/240/140"/>
+    </v-col>
+    <v-col cols="3" md="3" sm="6" xs="6">
+      <Product name="Smart Fridge" price="1000 RON" image="https://picsum.photos/240/140"/>
+    </v-col>
+    <v-col cols="3" md="3" sm="6" xs="6">
+      <Product name="Smart Fridge" price="1000 RON" image="https://picsum.photos/240/140"/>
+    </v-col>
+    <v-col cols="3" md="3" sm="6" xs="6">
+      <Product name="Smart Fridge" price="1000 RON" image="https://picsum.photos/240/140"/>
+    </v-col>
+    <v-col cols="3" md="3" sm="6" xs="6">
+      <Product name="Smart Fridge" price="1000 RON" image="https://picsum.photos/240/140"/>
+    </v-col>
+    <v-col cols="3" md="3" sm="6" xs="6">
+      <Product name="Smart Fridge" price="1000 RON" image="https://picsum.photos/240/140"/>
+    </v-col>
+    <v-col cols="3" md="3" sm="6" xs="6">
+      <Product name="Smart Fridge" price="1000 RON" image="https://picsum.photos/240/140"/>
+    </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
