dcc603a43a679dc0c4f897b72317fb8c5cee2db4
Ciprian Ursulean <ciprian.ursulean5@gmail.com>
2020-07-12 15:24:48 +0300

    Crafted product component

---
+++ src/components/Product.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card
+    class="mx-auto mt-5"
+    max-width="240"
+  >
+    <v-img
+      :src="image"
+      height="140px"
+    ></v-img>
+
+
+    <v-card-title class="accent--text">
+      {{ name }}
+    </v-card-title>
+
+    <v-card-actions>
+      <v-btn text icon><v-icon>local_mall</v-icon></v-btn>
+      <v-spacer></v-spacer>
+      <v-card-title class="primary--text justify-center">
+      {{ price }}
+      </v-card-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+
+  }),
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
-  <p>Homeee</p>
+  <v-col cols="12">
+    <Product name="Bormasina" price="123 RON" image="https://picsum.photos/240/140"/>
+    <Product name="Smart TV" price="234 RON" image="https://picsum.photos/240/140"/>
+  </v-col>
 </template>
 
 <script>
+import Product from "../components/Product.vue"
 
 export default {
-  
+  components: {
+    Product
+  }
 };
 </script>
